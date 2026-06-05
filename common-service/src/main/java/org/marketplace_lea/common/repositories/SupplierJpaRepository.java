3b7ecbef91d6313581d2ed205020ab378e1bcd6f
c_hkonan1 <harvey.konan@orange.com>
2026-06-05 21:44:10 +0000

    feat: Mise en place des API de configurations.

--- common-service/src/main/java/org/marketplace_lea/common/repositories/SupplierJpaRepository.java
+++ common-service/src/main/java/org/marketplace_lea/common/repositories/SupplierJpaRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 
+
 public interface SupplierJpaRepository extends JpaRepository<SupplierV2Entity, Long>, JpaSpecificationExecutor<SupplierV2Entity> {
     @Query(value = "SELECT COUNT(supplier.id) FROM SupplierV2 supplier")
     long countSuppliers();
